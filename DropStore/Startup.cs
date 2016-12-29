70fd6281cbace8611a4c6d1d9b6707a81992c03b
MAKINGWAVES\mareri <erik.martinsen@makingwaves.com>
2016-12-29 13:46:54 +0100

    Add project files.

---
+++ DropStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DropStore.Startup))]
+namespace DropStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
